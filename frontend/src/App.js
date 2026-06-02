1cc563c7b307b541b0d477b5f655e9d79db01d6f
Carl Fampo <cfampo@iu.edu>
2026-06-01 22:31:35 -0400

    Worked on login and singup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+
 
 // pages & components
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
@@ -12,6 +15,12 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+          </Routes>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
           </Routes>
         </div>
       </BrowserRouter>
